e3a2514c486f47bf419dab5d0f71ff7708014d55
Robin Troyer <troyerrobin@gmail.com>
2020-12-16 06:58:39 +0100

    ausgebessert

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<html class="no-js" lang="de">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -11,102 +11,138 @@
         <link rel="stylesheet" href="./assets/css/foundation.css" />
         <link rel="stylesheet" href="./assets/dist/style.css">
         <!-- <script src="./assets/js/app.js"></script> -->
+        <script>
+          function showNav() {
+            var nav = document.getElementById('nav_div');
+            if (nav.className.includes('closed')) {
+              nav.className = nav.className.replace('closed', 'open')
+            } else if (nav.className.includes('open')) {
+              nav.className = nav.className.replace('open', 'closed');
+            }
+          }
+          function buttonPressed() {
+            var header = document.getElementById('header');
+            if (!header.className.includes('hidden')) {
+              header.className = header.className + 'hidden';
+            }
+          }
+          // var header = document.getElementById('header');
+          // $(document).ready(function(){
+          //   console.log($(document).scrollTop());
+          // })
+
+          function pageScrolled() {
+            var header = document.getElementById('header');
+            var nav = document.getElementById('pc_navigation');
+            if ($(document).scrollTop() > 0) {
+              // console.log($(document).scrollTop());
+              if (!header.className.includes('hidden')) {
+                header.className = header.className + 'hidden';
+              }
+              if (!nav.className.includes('header_hidden')) {
+                nav.className = nav.className + ' header_hidden'
+              }
+            } else {
+              if (header.className.includes('hidden')) {
+                header.className = header.className.replace('hidden', '');
+              }
+              if (nav.className.includes('header_hidden')) {
+                nav.className = nav.className.replace(' header_hidden', '');
+              }
+            }
+            // var pc_nav = document.getElementById('pc_navigation');
+            // var sticky = pc_nav.offsetTop;
+            // if ()
+          }
+
+        </script>
     </head>
-    <script>
-      function showNav() {
-        var nav = document.getElementById('nav_div');
-        if (nav.className.includes('closed')) {
-          nav.className = nav.className.replace('closed', 'open')
-        } else if (nav.className.includes('open')) {
-          nav.className = nav.className.replace('open', 'closed');
-        }
-      }
-      function buttonPressed() {
-        var header = document.getElementById('header');
-        if (!header.className.includes('hidden')) {
-          header.className = header.className + 'hidden';
-        }
-      }
-      // var header = document.getElementById('header');
-      // $(document).ready(function(){
-      //   console.log($(document).scrollTop());
-      // })
-
-      function pageScrolled() {
-        var header = document.getElementById('header');
-        var nav = document.getElementById('pc_navigation');
-        if ($(document).scrollTop() > 0) {
-          // console.log($(document).scrollTop());
-          if (!header.className.includes('hidden')) {
-            header.className = header.className + 'hidden';
-          }
-          if (!nav.className.includes('header_hidden')) {
-            nav.className = nav.className + ' header_hidden'
-          }
-        } else {
-          if (header.className.includes('hidden')) {
-            header.className = header.className.replace('hidden', '');
-          }
-          if (nav.className.includes('header_hidden')) {
-            nav.className = nav.className.replace(' header_hidden', '');
-          }
-        }
-        // var pc_nav = document.getElementById('pc_navigation');
-        // var sticky = pc_nav.offsetTop;
-        // if ()
-      }
-
-    </script>
+    
     <body onscroll="pageScrolled()">
         <div id="header">
           <div id="header_logo">
-            <img src="./src/images/logo-putzenhof-gold.svg" alt="Putzenhof">
-            <img class="logo_alt" src="./src/images/Screenshot_5.png" alt="Putzenhof">
+            <img src="./src/images/logo-putzenhof-gold.svg" alt="Putzenhof" />
+            <img class="logo_alt" src="./src/images/Screenshot_5.png" alt="Putzenhof" />
           </div>
           <div id="header_main">
             <div id="header_contact">
               <div id="phone">
                 <!-- <img src="./src/images/phone.svg" alt="Telefon" id="phone"> -->
-                <img src="./src/images/phone.svg" alt="Telefon">
+                <img src="./src/images/phone.svg" alt="Telefon" />
                 <span id="number">+43 (0) 664 2868936</span>
               </div>
-              <img src="./src/images/envelope.svg" alt="E-Mail" id="email">
-              <img src="./src/images/location.svg" alt="Lage" id="location">
+              <img src="./src/images/envelope.svg" alt="E-Mail" id="email" />
+              <img src="./src/images/location.svg" alt="Lage" id="location" />
             </div>
             <div id="header_nav" onclick="showNav()">
               <span>MENÜ</span>
-              <img id="bars" src="./src/images/bars.svg" alt="bars">
+              <img id="bars" src="./src/images/bars.svg" alt="bars" />
             </div>
           </div>
         </div>
         <div id="pc_navigation" class="pc_nav">
-          <ul>
-            <div></div>
-              <button class="button" type="button" data-toggle="example-dropdown-1">Der Putzenhof</button>
-              <div class="dropdown-pane" id="example-dropdown-1" data-dropdown data-hover="true" data-hover-pane="true">
-                <li><a href="#">Lage & Anreise</a></li>
-                <li><a href="#">Impressionen</a></li>
-              </div>
-              <button class="button" type="button" data-toggle="example-dropdown-2">Appartements & Preise</button>
+          <ul class="dropdown menu" data-dropdown-menu>
+            <!-- <div></div> -->
+              <!-- <button class="button" type="button" data-toggle="example-dropdown-1">Der Putzenhof</button> -->
+              <!-- <li class="button navbtn" data-toggle="example-dropdown-1">Der Putzenhof</li> -->
+
+
+
+              
+              <!-- <li class="button navbtn" data-toggle="example-dropdown-2">Appartements & Preise</li>
               <div class="dropdown-pane" id="example-dropdown-2" data-dropdown data-hover="true" data-hover-pane="true">
                 <li><a href="#">Appartement "Melchior Putz"</a></li>
                 <li><a href="#">Appartement "Großglockner</a></li>
                 <li><a href="#">Preise</a></li>
                 <li><a href="#">Anfrage</a></li>
-              </div>
-              <button class="button" type="button" data-toggle="example-dropdown-3">Region & Aktiv</button>
+              </div> -->
+              <!-- <li class="button navbtn" data-toggle="example-dropdown-2">
+                <a class="pcnav_a" href="#">Appartements & Preise</a>
+          
+              </li> -->
+              <li>
+                <a class="pcnav_a" href="#">Der Putzenhof</a>
+                <ul class="menu">
+                  <li><a href="#">Lage & Anreise</a></li>
+                  <li><a href="#">Impressionen</a></li>
+                </ul>
+              </li>
+              <li>
+                <a class="pcnav_a" href="#">Appartements & Preise</a>
+                <ul class="menu">
+                  <li><a href="#">Appartement "Melchior Putz"</a></li>
+                  <li><a href="#">Appartement "Großglockner</a></li>
+                  <li><a href="#">Preise</a></li>
+                  <li><a href="#">Anfrage</a></li>
+                </ul>
+              </li>
+              <li>
+                <a class="pcnav_a" href="#">Region & Aktiv</a>
+                <ul class="menu">
+                  <li><a href="#">Nationalpark Hohe Tauern & Großglockner</a></li>
+                  <li><a href="#">Sommer</a></li>
+                  <li><a href="#">Winter</a></li>
+                </ul>
+              </li>
+              <li>
+                <a class="pcnav_a" href="#">Anfrage</a>
+              </li>
+
+
+<!-- 
+              <li class="button navbtn" data-toggle="example-dropdown-3">Region & Aktiv</li>
               <div class="dropdown-pane" id="example-dropdown-3" data-dropdown data-hover="true" data-hover-pane="true">
                 <li><a href="#">Nationalpark Hohe Tauern & Großglockner</a>
                 <li><a href="#">Sommer</a></li>
                 <li><a href="#">Winter</a></li>
               </div>
-              <button class="button" type="button">Anfrage</button>
+              <li class="button navbtn">Anfrage</li> -->
               
             <!-- <li class="no-hover" onmouseover="openList()">Der Putzenhof</li>
             <li class="no-hover" onmouseover="openList()">Appartements & Preise</li>
             <li class="no-hover" onmouseover="openList()">Region & Aktiv</li>
             <li class="no-hover" onmouseover="openList()">Anfrage</li> -->
-            <div></div>
+            <!-- <div></div> -->
           </ul>
         </div>
         <div id="nav_div" class="closed">
@@ -171,31 +207,31 @@
           <div class="row">
             <div class="small-12 medium-12 large-8 columns">
               <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
-                <ul class="orbit-container">
+                <div class="orbit-container">
                   <!-- <button class="orbit-previous">&#9664;&#xFE0E;</button> -->
                   <button class="orbit-previous">
-                    <img src="./src/images/2x/baseline_keyboard_arrow_left_black_18dp.png" alt="Zurück">
+                    <img src="./src/images/2x/baseline_keyboard_arrow_left_black_18dp.png" alt="Zurück" />
                   </button>
                   <button class="orbit-next">
-                    <img src="./src/images/2x/baseline_keyboard_arrow_right_black_18dp.png" alt="Vor">
+                    <img src="./src/images/2x/baseline_keyboard_arrow_right_black_18dp.png" alt="Vor" />
                   </button>
-                  <li class="is-active orbit-slide">
-                    <img class="orbit-image" src="./src/images/csm_putzenhof-aussen-01_d1682a7261.jpg" alt="Space">
+                  <div class="is-active orbit-slide">
+                    <img class="orbit-image" src="./src/images/csm_putzenhof-aussen-01_d1682a7261.jpg" alt="Space" />
                     <!-- <figcaption class="orbit-caption">Space, the final frontier.</figcaption> -->
-                  </li>
-                  <li class="orbit-slide">
-                    <img class="orbit-image" src="./src/images/csm_putzenhof-felsch-9878-HDR_94b7cfc87f.jpg" alt="Space">
+                  </div>
+                  <div class="orbit-slide">
+                    <img class="orbit-image" src="./src/images/csm_putzenhof-felsch-9878-HDR_94b7cfc87f.jpg" alt="Space" />
                     <!-- <figcaption class="orbit-caption">Lets Rocket!</figcaption> -->
-                  </li>
-                  <li class="orbit-slide">
-                    <img class="orbit-image" src="./src/images/csm_putzenhof-felsch-0400-HDR_a707e498b9.jpg" alt="Space">
+                  </div>
+                  <div class="orbit-slide">
+                    <img class="orbit-image" src="./src/images/csm_putzenhof-felsch-0400-HDR_a707e498b9.jpg" alt="Space" />
                     <!-- <figcaption class="orbit-caption">Encapsulating</figcaption> -->
-                  </li>
-                  <li class="orbit-slide">
-                    <img class="orbit-image" src="./src/images/csm_putzenhof-02_b9e8f3b943.jpg" alt="Space">
+                  </div>
+                  <div class="orbit-slide">
+                    <img class="orbit-image" src="./src/images/csm_putzenhof-02_b9e8f3b943.jpg" alt="Space" />
                     <!-- <figcaption class="orbit-caption">Outta This World</figcaption> -->
-                  </li>
-                </ul>
+                  </div>
+        </div>
                 <!-- <nav class="orbit-bullets">
                   <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
                   <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
@@ -240,7 +276,7 @@
 
           <div id="mini_logo_div">
             <div id="mini_logo">
-              <img src="./src/images/Screenshot_4.png" alt="">
+              <img src="./src/images/Screenshot_4.png" alt="" />
             </div>
           </div>
 
@@ -257,41 +293,41 @@
           <div class="cards">
             <div></div>
             <div class="card">
-              <img src="./src/images/csm_putzenhof-appartement-03_6de71fe36f.png">
+              <img src="./src/images/csm_putzenhof-appartement-03_6de71fe36f.png" alt="Appartements" />
               <div class="card-section">
                 <h4>Appartements</h4>
                 <p>gediegen Wohnen im Putzenhof</p>
-                <img class="arrow" src="./src/images/arrow-right.svg" alt="">
+                <img class="arrow" src="./src/images/arrow-right.svg" alt="arrow" />
               </div>
             </div>
             <div></div>
             <div></div>
             <div class="card">
-              <img src="./src/images/Screenshot_3.png">
+              <img src="./src/images/Screenshot_3.png" alt="Putzenhof" />
               <div class="card-section">
                 <h4>Der Putzenhof</h4>
                 <p>Geschichte, Lage und Region</p>
-                <img class="arrow" src="./src/images/arrow-right.svg" alt="">
+                <img class="arrow" src="./src/images/arrow-right.svg" alt="arrow" />
               </div>
             </div>
             <div></div>
             <div></div>
             <div class="card">
-              <img src="./src/images/Screenshot_1.png">
+              <img src="./src/images/Screenshot_1.png" alt="Sommerurlaub" />
               <div class="card-section">
                 <h4>Sommerurlaub</h4>
                 <p>Sommer am Großglockner</p>
-                <img class="arrow" src="./src/images/arrow-right.svg" alt="">
+                <img class="arrow" src="./src/images/arrow-right.svg" alt="arrow" />
               </div>
             </div>
             <div></div>
             <div></div>
             <div class="card">
-              <img src="./src/images/Screenshot_2.png">
+              <img src="./src/images/Screenshot_2.png" alt="Winter" />
               <div class="card-section">
                 <h4>Winter</h4>
                 <p>Winter im Nationalpark Hohe Tauern</p>
-                <img class="arrow" src="./src/images/arrow-right.svg" alt="">
+                <img class="arrow" src="./src/images/arrow-right.svg" alt="arrow" />
               </div>
             </div>
             <div></div>
@@ -327,7 +363,7 @@
         <footer>
           <div class="upper">
             <div class="contact">
-              <img src="./src/images/logo-putzenhof-gold.svg" alt="Putzenhof">
+              <img src="./src/images/logo-putzenhof-gold.svg" alt="Putzenhof" />
               <p>Putzenhof Appartements</p>
               <br />
               <p>Am Putzenhof</p>
@@ -345,13 +381,13 @@
             <div style="border-left:2px solid grey;"></div>
             <div class="nationalpark">
               <p class="header">Urlaub im Nationalpark Hohe Tauern</p>
-              <p>Urlaub inmitten der Hohen Tauern buchen. Fragen Sie unverbindlich an.</h4>
+              <p>Urlaub inmitten der Hohen Tauern buchen. Fragen Sie unverbindlich an.</p>
               <button>Jetzt anfragen</button>
             </div>
           </div>
           <div class="images">
-            <img src="./src/images/logo-stgeorg.png" alt="St. Georg">
-            <img src="./src/images/csm_rz-alpenh-logo_f77e6ef5ed.png" alt="Großglockner Kaiser Franz-Josefs-Höhe">
+            <img src="./src/images/logo-stgeorg.png" alt="St. Georg" />
+            <img src="./src/images/csm_rz-alpenh-logo_f77e6ef5ed.png" alt="Großglockner Kaiser Franz-Josefs-Höhe" />
           </div>
           <div class="lower">
             <a href="#">Impressum & AGB</a>
